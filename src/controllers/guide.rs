2275f581b13b7685b460e50747e256d8f7964b9b
Masanari Mizuno <mznms@primu.la>
2024-06-21 20:15:48 +0900

    add an errors section

--- src/controllers/guide.rs
+++ src/controllers/guide.rs
@@ -1,14 +1,25 @@
 #![allow(clippy::unused_async)]
 use axum::debug_handler;
 use loco_rs::prelude::*;
-use axum::debug_handler;
-
 
 #[debug_handler]
 pub async fn echo(req_body: String) -> String {
     req_body
 }
 
+/// A function that returns a "hello" response.
+///
+/// # Arguments
+///
+/// * `State(_ctx)` - The application context.
+///
+/// # Returns
+///
+/// * A `Result` containing a `Response` with the text "hello" if successful.
+///
+/// # Errors
+///
+/// This function will return an error if there is an issue with creating the response.
 #[debug_handler]
 pub async fn hello(State(_ctx): State<AppContext>) -> Result<Response> {
     // do something with context (database, etc)
