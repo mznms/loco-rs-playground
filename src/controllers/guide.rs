7c05b6da607347a87101abf11bcc19d736df1767
Masanari Mizuno <mznms@primu.la>
2024-06-21 20:27:13 +0900

    make `hello` fn not pub

--- src/controllers/guide.rs
+++ src/controllers/guide.rs
@@ -7,21 +7,8 @@ pub async fn echo(req_body: String) -> String {
     req_body
 }
 
-/// A function that returns a "hello" response.
-///
-/// # Arguments
-///
-/// * `State(_ctx)` - The application context.
-///
-/// # Returns
-///
-/// * A `Result` containing a `Response` with the text "hello" if successful.
-///
-/// # Errors
-///
-/// This function will return an error if there is an issue with creating the response.
 #[debug_handler]
-pub async fn hello(State(_ctx): State<AppContext>) -> Result<Response> {
+async fn hello(State(_ctx): State<AppContext>) -> Result<Response> {
     // do something with context (database, etc)
     format::text("hello")
 }
